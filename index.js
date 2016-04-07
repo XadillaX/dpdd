748aabc223638972e3b6d6ad1e12f7d43b746b8f
XadillaX <admin@xcoder.in>
2016-04-07 13:30:16 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * XadillaX created at 2016-04-07 13:26:14 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+exports.ConfigSchema = require("./lib/config_schema");
+exports.DDNS = require("./lib/ddns");
+exports.IpGetter = require("./lib/ip_getters");
+exports.Record = require("./lib/record");
