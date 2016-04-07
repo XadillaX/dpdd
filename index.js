4cf41ae4f1bd9d6357024704d5f2c1e16a457df2
XadillaX <admin@xcoder.in>
2016-04-07 16:17:31 +0800

    add jsdoc support

--- index.js
+++ index.js
@@ -4,9 +4,32 @@
  * Copyright (c) 2016 Souche.com, all rights
  * reserved.
  */
+/**
+ * dpdd module.
+ * @module ddns
+ */
 "use strict";
 
+/**
+ * @member {Class} - The ConfigSchema class.
+ * @see ConfigSchema
+ */
 exports.ConfigSchema = require("./lib/config_schema");
+
+/**
+ * @member {Class} - the DDNS class.
+ * @see DDNS
+ */
 exports.DDNS = require("./lib/ddns");
+
+/**
+ * @member {Object.<Function>} - the IP getters.
+ * @see module:IpGetter
+ */
 exports.IpGetter = require("./lib/ip_getters");
+
+/**
+ * @member {Class} - the Record class.
+ * @see Record
+ */
 exports.Record = require("./lib/record");
